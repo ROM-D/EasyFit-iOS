2d57e89636d1ca6ba2e6f0dbc1d1e5d5a8cecbd0
Rodrigo Cravioto <a01748172@itesm.mx>
2021-11-18 21:08:17 -0800

    Initial Commit

---
+++ EasyFitIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyFitIOS
+//
+//  Created by user197752 on 11/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
